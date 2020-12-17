01df004ecdc9f868cab3efaf100e0e3ec06a4540
Arup Chowdhary <arup.chowdhary@gmail.com>
2020-12-17 11:58:45 -0700

    added html response

--- webserver/node/index.js
+++ webserver/node/index.js
@@ -4,5 +4,13 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end('<h1>Home </h1>');
+    }
     console.log("something is running");
-}).listen(5000, () => console.log(`shh, I am listening`));
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(5000, () => console.log(`shh, I am listening at ${PORT} port`));
